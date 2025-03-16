8bfcf4bb39cfb660f0db06d498de28f3c304bbac
stealthmate <stealthmate1233@gmail.com>
2025-03-17 00:21:40 +0900

    initiative(v1): rewrite example to run with newest version of package

--- initiatives/v1/main.py
+++ initiatives/v1/main.py
@@ -1,87 +1,70 @@
-import enum
 import random
 
 import pydantic
+import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 
-from orgsim import v1
+from orgsim.v1.game import Game, Factory, IndividualStats, IndividualStrategy, Seed
+from orgsim.v1.variants import individual
 
 random.seed(0)
 
 N_PERIODS = 200
 N_DAYS_IN_PERIOD = 30
 INITIAL_ORG_WEALTH = 1_000_000
-ORG_PRODUCTIVITY = 1.0
+ORG_PRODUCTIVITY = 2.0
 PRODUCTION_TO_VALUE_COEF = 0.1
 MAX_INVEST_COEF = 1.0
 
 
-class IndividualType(enum.Enum):
-    SLAVE = 0
-
-
 class IndividualSeed(pydantic.BaseModel):
-    type_: IndividualType
+    pass
 
 
-class OrgType(enum.Enum):
-    SHITHOLE = 0
-
-
-class OrgSeed(pydantic.BaseModel):
-    type_: OrgType
-
-
-class Factory(v1.game.seed.Factory[IndividualSeed, OrgSeed]):
+class FactoryImpl(Factory[IndividualSeed]):
     def __init__(self) -> None:
         self._identity_counter = 0
 
     def create_individual(
         self, seed: IndividualSeed
-    ) -> tuple[str, v1.game.base.Individual, v1.game.state.IndividualData]:
+    ) -> tuple[str, IndividualStats, IndividualStrategy]:
         self._identity_counter += 1
         return (
             str(self._identity_counter),
-            v1.variants.individual.Slave(),
-            v1.game.state.IndividualData(
-                accumulated_value=0,
-                wealth=1_000_000,
-                base_production=10_000,
+            IndividualStats(
+                score=0,
+                wealth=0,
+                unit_production=10_000,
                 salary=300_000,
-                cost_of_living=300_000,
-                contribution=0,
+                cost_of_living=200_000,
             ),
+            individual.Slave(),
         )
 
-    def create_org(self, seed: OrgSeed) -> v1.game.base.Org:
-        return v1.variants.org.Shithole()
 
-
-def generate_seed(org_type: OrgType) -> v1.game.seed.Seed:
-    return v1.game.seed.Seed(
+def generate_seed() -> Seed:
+    return Seed(
         periods=N_PERIODS,
         days_in_period=N_DAYS_IN_PERIOD,
-        initial_individuals=[
-            IndividualSeed(type_=IndividualType.SLAVE) for _ in range(10)
-        ],
+        initial_individuals=[IndividualSeed() for _ in range(10)],
         initial_org_wealth=INITIAL_ORG_WEALTH,
         org_productivity=ORG_PRODUCTIVITY,
-        org_seed=OrgSeed(type_=org_type),
         production_to_value_coef=PRODUCTION_TO_VALUE_COEF,
         max_invest_coef=MAX_INVEST_COEF,
     )
 
 
-seed = generate_seed(OrgType.SHITHOLE)
-game = v1.game.Game.from_seed(seed, Factory())
+seed = generate_seed()
+game = Game.from_seed(seed, FactoryImpl())
 score = game.play()
 for k, v in score.model_dump().items():
     print(k.rjust(30), f"{v: 10.0f}")
 
-metrics = game._game.metrics
+metrics = game._game._state.metrics._metrics
 pop = metrics.get_fiscal_series("population")
 
-wealth_series = list(metrics.get_series_in_class("individual_wealth"))
+wealth_series = list(metrics.get_series_in_class("individual_unit_production"))
 df = pd.concat(
     [x[0] for x in wealth_series], keys=[x[1]["identity"] for x in wealth_series]
 )
@@ -92,6 +75,6 @@ df = (
     .max()
     .reset_index()
 )
-# print(df.head())
-# sns.lineplot(df, x='period', y='value', hue='identity')
-# plt.show()
+print(df.head())
+sns.lineplot(df, x="period", y="value", hue="identity")
+plt.show()
